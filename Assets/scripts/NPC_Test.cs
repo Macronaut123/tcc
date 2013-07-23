73033be8189bd95f377f6c1b4b75abfee1c58df6
Macronaut123 <marcos.vinicius.eua@gmail.com>
2013-07-22 23:58:51 -0300

    melhorando sistema de descricao e agenda

--- Assets/scripts/NPC_Test.cs
+++ Assets/scripts/NPC_Test.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class NPC_Test : MonoBehaviour {
 
-    public GameObject TargetObject;
+    private GameObject TargetObject;
 
     private int age;
     private string name;
@@ -12,11 +12,15 @@ public class NPC_Test : MonoBehaviour {
     private int[] dailyRoutine;
     private bool[] dailyRoutineCondition;
 
+    private int TargetID;
+
 
     private bool showDescription;
 
 	// Use this for initialization
 	void Start () {
+
+        TargetObject = GameObject.FindGameObjectWithTag("NPC_armazem");
 
         print("Idade do Objeto: " + TargetObject.GetComponent<NPC_Info>().age);
         print("Nome do Objeto: " + TargetObject.GetComponent<NPC_Info>().name);
@@ -38,13 +42,34 @@ public class NPC_Test : MonoBehaviour {
 	void Update () {
 
         if (Input.GetKeyDown("u")) { if (showDescription) { showDescription = false; } else { showDescription = true; } }
+
+        if (Input.GetKeyDown("i")) { TargetID = 1; print(TargetID); }
+        if (Input.GetKeyDown("o")) { TargetID = 2; print(TargetID); }
+
+        switch (TargetID){
+            case 1:
+                TargetObject = GameObject.FindGameObjectWithTag("NPC_armazem");
+                break;
+            case 2:
+                TargetObject = GameObject.FindGameObjectWithTag("NPC_guardaforte");
+                break;
+        }
+
+        age = TargetObject.GetComponent<NPC_Info>().age;
+        name = TargetObject.GetComponent<NPC_Info>().name;
+        subinfo = TargetObject.GetComponent<NPC_Info>().subinfo;
+        description = TargetObject.GetComponent<NPC_Info>().description;
+        dailyRoutine = TargetObject.GetComponent<NPC_Info>().dailyRoutine;
+        dailyRoutineCondition = TargetObject.GetComponent<NPC_Info>().dailyRoutineCondition;
 	
 	}
 
     void OnGUI(){
 
+        GUI.skin.box.wordWrap = true;
+
         if (showDescription){
-            GUI.Box(new Rect(40, 40, 400, 100), "\nIdade: " + age + "\nNome: " + name + "\nClasse: " + subinfo + "\nDescrição: " + description);
+            GUI.Box(new Rect(40, 40, 350, 150), "\nIdade: " + age + "\nNome: " + name + "\nClasse: " + subinfo + "\nDescrição: " + description);
             GUI.Box(new Rect(440, 15, 80, 275), "");
             for (int i = 0; i < dailyRoutine.Length; i++) {
                 if (dailyRoutineCondition[i]){
