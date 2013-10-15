5c53ae7a800b381fd3c6f760321991897697b99e
Macronaut123 <marcos.vinicius.eua@gmail.com>
2013-10-15 20:40:42 -0300

    inserindo versao mais nova com animacoes

--- Assets/scripts/Game Scripts/AnimatorControl.cs
+++ Assets/scripts/Game Scripts/AnimatorControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class AnimatorControl : MonoBehaviour {
 
-    private Animator animControl;
+    public Animator animControl;
     public bool Anim1;
     public bool Anim2;
     public bool Anim3;
@@ -12,13 +12,13 @@ public class AnimatorControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        animControl = gameObject.GetComponent<Animator>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Anim1) {
-            animControl.SetBool("Idle", false);
+            animControl.SetBool("Idle", true);
             animControl.SetBool("Andando", false);
             animControl.SetBool("FalandoNormal", false);
             animControl.SetBool("Correndo", false);
@@ -50,9 +50,5 @@ public class AnimatorControl : MonoBehaviour {
     }
 
     void OnGUI() { 
-        if (GUI.Button(new Rect(40,40,130,20),"IDLE")){ Anim1 = true; Anim2 = false; Anim3 = false; Anim4 = false; }
-        if (GUI.Button(new Rect(40, 60, 130, 20), "ANDANDO")) { Anim1 = false; Anim2 = true; Anim3 = false; Anim4 = false; }
-        if (GUI.Button(new Rect(40, 80, 130, 20), "FALANDO NORMAL")) { Anim1 = false; Anim2 = false; Anim3 = true; Anim4 = false; }
-        if (GUI.Button(new Rect(40, 100, 130, 20), "CORRENDO")) { Anim1 = false; Anim2 = false; Anim3 = false; Anim4 = true; }
     }
 }
