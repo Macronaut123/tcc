31b3a5e3bf994b2b71ed1e169d085fc55a4048db
Macronaut123 <marcos.vinicius.eua@gmail.com>
2013-10-29 00:07:06 -0200

    inserindo nova interface

---
+++ Assets/scripts/NGUI Scripts/InitScreen/SoundSystem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundSystem : MonoBehaviour {
+
+    private UIToggle toggleControl;
+
+	// Use this for initialization
+	void Start () {
+        toggleControl = gameObject.GetComponent<UIToggle>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick() {
+        if (toggleControl.value == false) { AudioListener.pause = true; } else { AudioListener.pause = false; }
+    }
+}
---
+++ Assets/scripts/NGUI Scripts/InitScreen/StartGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    public UILabel buttonText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnClick() {
+        //COMEÇAR O JOGO//
+    }
+}
